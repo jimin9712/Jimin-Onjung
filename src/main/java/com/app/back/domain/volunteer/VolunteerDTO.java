37d3cb0b86e5826bc737941578a74dab4eed5995
hmk4079 <hmk4079@gmail.com>
2024-10-23 13:58:36 +0900

    10-23/13:58

--- src/main/java/com/app/back/domain/volunteer/VolunteerDTO.java
+++ src/main/java/com/app/back/domain/volunteer/VolunteerDTO.java
@@ -1,14 +1,15 @@
 package com.app.back.domain.volunteer;
 
 
+
 import lombok.*;
 import org.springframework.stereotype.Component;
 
 @Component
-@Getter @ToString
+@Getter @Setter @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class VolunteerVO {
+public class VolunteerDTO {
     private Long id;
     private int vtRecruitmentCount;
     private String postTitle;
@@ -20,4 +21,4 @@ public class VolunteerVO {
     private String updatedDate;
 
 
-}
+public VolunteerVO toVO(){return new VolunteerVO(id,vtRecruitmentCount,postTitle,memberNickName,postViewCount,postType,postSummary,createdDate,updatedDate);}}
--- src/main/resources/sql/vt/vt.sql
+++ src/main/resources/sql/vt/vt.sql
@@ -1,7 +1,9 @@
 create table tbl_vt(
     id bigint unsigned primary key ,
+    vt_recruitment_count smallint not null,
     constraint fk_vt_post foreign key(id)
                    references tbl_post(id)
 );
+_recruitment_count smallint not null;
 
 drop table tbl_vt;
