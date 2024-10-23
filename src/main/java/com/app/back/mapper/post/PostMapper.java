6fc568f7f9dd1c38c9e2d81ba2f8ae3a32cb2fb4
dev-resetok <746aa1357@gmail.com>
2024-10-23 17:11:52 +0900

    postMaper.xml working

--- src/main/java/com/app/back/mapper/post/PostMapper.java
+++ src/main/java/com/app/back/mapper/post/PostMapper.java
@@ -1,4 +1,26 @@
 package com.app.back.mapper.post;
 
+import com.app.back.domain.post.PostVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
 public interface PostMapper {
+    //    게시글 작성
+    public void insert(PostVO postVO);
+
+    // ID로 게시글 조회
+    public PostVO selectById(Long id);
+
+    // ID로 게시글 수정
+    public void updateById(PostVO postVO);
+
+    // ID로 게시글 삭제
+    public void deleteById(Long id);
+
+    // 전체 게시글 조회 (기존 메소드)
+    public List<PostVO> selectAll();
+
+    //    조회수 증가
+    public void increaseViewCountById(Long id);
 }
--- src/main/java/com/app/back/mapper/review/ReviewMapper.java
+++ src/main/java/com/app/back/mapper/review/ReviewMapper.java
@@ -1,4 +1,25 @@
 package com.app.back.mapper.review;
 
+import com.app.back.domain.post.Pagination;
+import com.app.back.domain.review.ReviewDTO;
+import com.app.back.domain.review.ReviewVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
 public interface ReviewMapper {
+    // 추가
+    public void insert(ReviewVO reviewVO);
+    // 조회
+    public Optional<ReviewVO> selectById(Long id);
+    // 전체 조회
+    public List<ReviewDTO> selectAll(Pagination pagination);
+    // 전체 개수
+    public int selectCount();
+    // 수정
+    public void update(ReviewDTO reviewDTO);
+    // 삭제
+    void deleteById(Long id);
 }
