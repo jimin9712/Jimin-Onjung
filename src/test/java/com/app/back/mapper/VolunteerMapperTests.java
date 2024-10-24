15f49f088a552232ae3062badb817ad762999b7f
hmk4079 <hmk4079@gmail.com>
2024-10-24 17:26:30 +0900

    init

--- src/test/java/com/app/back/mapper/VolunteerMapperTests.java
+++ src/test/java/com/app/back/mapper/VolunteerMapperTests.java
@@ -19,15 +19,11 @@ public class VolunteerMapperTests {
         VolunteerDTO volunteerDTO = new VolunteerDTO();
         volunteerDTO.setPostType("1");
         volunteerDTO.setPostTitle("봉사활동구인제목1");
-        volunteerDTO.setPostSummary("봉사활동축약내용1");
         volunteerDTO.setRecruitmentCount(10);
-        volunteerDTO.setVtSDate("2024-10-25");
-        volunteerDTO.setVtEDate("2024-11-25");
+        volunteerDTO.setVtSDate("2024-11-25");
+        volunteerDTO.setVtEDate("2025-11-25");
         volunteerDTO.setPostContent("봉사활동전체내용1");
-        volunteerDTO.setAttachmentFileName("첨부파일1");
-        volunteerDTO.setAttachmentFilePath("123");
-        volunteerDTO.setAttachmentFileSize("300");
-        volunteerDTO.setAttachmentFiletype("일반");
+        volunteerDTO.setMemberId(2L);
 
         volunteerMapper.insert(volunteerDTO.toVO());
         log.info("봉사활동구인글이 작성 되었습니다: " + volunteerDTO);
