cd4a1561bd7a8dc200e350630ab6b851bdd4e843
dev-resetok <746aa1357@gmail.com>
2024-10-29 02:50:17 +0900

    attachment working

--- src/main/java/com/app/back/repository/attachment/AttachmentDAO.java
+++ src/main/java/com/app/back/repository/attachment/AttachmentDAO.java
@@ -6,12 +6,36 @@ import com.app.back.mapper.attachment.AttachmentMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class AttachmentDAO {
     private final AttachmentMapper attachmentMapper;
 
-//    파일추가
-    public void save(AttachmentVO attachmentVO){attachmentMapper.insert(attachmentVO);}
+//    파일 추가
+    public void save(AttachmentVO attachmentVO){
+        attachmentMapper.insert(attachmentVO);
+    }
+
+//    파일 조회
+    public AttachmentVO findById(Long id){
+        return attachmentMapper.selectById(id);
+    }
+
+//    파일 목록
+    public List<AttachmentVO> findAll(Long postId){
+        return attachmentMapper.selectAll(postId);
+    }
+
+//    파일 삭제
+    public void delete(Long id){
+        attachmentMapper.delete(id);
+    }
+
+//    게시글 삭제 시, 해당 파일 전체 삭제
+    public void deleteByPostId(Long postId){
+        attachmentMapper.deleteByPostId(postId);
+    }
 
 }
---
+++ src/main/resources/static/js/attachment/review-attachment-service.js
@@ -0,0 +1,13 @@
+const reviewAttachmentReview = (() => {
+    // 파일 업로드
+    const upload = async (formData) => {
+        const response = await fetch("/review/review-write", {
+            method: "post",
+            body: formData
+        });
+        const attachmentFileName = await response.json();
+        return attachmentFileName;
+    }
+
+    return {upload: upload};
+})()
