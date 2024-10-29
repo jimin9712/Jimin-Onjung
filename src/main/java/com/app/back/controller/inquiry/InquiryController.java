f5f75eaa1bca572ea3d09fab9385ad3b0bf42119
yunjuhyeon <ka06251@naver.com>
2024-10-29 17:20:49 +0900

    clear

--- src/main/java/com/app/back/controller/inquiry/InquiryController.java
+++ src/main/java/com/app/back/controller/inquiry/InquiryController.java
@@ -26,11 +26,39 @@ public class InquiryController {
 
     @GetMapping
     public String getList(Pagination pagination, Search search, Model model, HttpServletRequest request) {
+        log.info("Controller - getList() 호출됨");
+        log.info("페이지네이션 정보: {}", pagination);
+        log.info("검색 조건: {}", search);
+
+        // page가 null인 경우 기본값으로 1을 설정
+        if (pagination.getPage() == null) {
+            pagination.setPage(1);
+        }
+
+        if (pagination.getOrder() == null) {
+            pagination.setOrder("created_date desc, n.id desc"); // 기본 정렬 기준
+        }
+        if (search.getKeyword() != null) {
+            pagination.setTotal(inquiryService.getTotalWithSearch(search));
+        } else {
+            pagination.setTotal(inquiryService.getTotal());
+        }
+
+        pagination.progress();
+
         List<InquiryDTO> inquiries = inquiryService.getList(pagination, search);
+        System.out.println("조회된 데이터: " + inquiries);
+
+
         model.addAttribute("inquiries", inquiries);
+        model.addAttribute("pagination", pagination);
+        model.addAttribute("search", search);
+
 
         return "admin";
     }
 
 
+
 }
+
