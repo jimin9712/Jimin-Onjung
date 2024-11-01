7751d335688a39844fae1ddb48def4b6568d08a7
hmk4079 <hmk4079@gmail.com>
2024-11-01 19:57:00 +0900

    init

--- src/main/java/com/app/back/controller/volunteer/VolunteerController.java
+++ src/main/java/com/app/back/controller/volunteer/VolunteerController.java
@@ -1,28 +1,25 @@
 package com.app.back.controller.volunteer;
 
-import com.app.back.domain.review.ReviewDTO;
 import com.app.back.domain.volunteer.Pagination;
 import com.app.back.domain.volunteer.VolunteerDTO;
+import com.app.back.service.post.PostService;
 import com.app.back.service.volunteer.VolunteerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller // 이 클래스가 컨트롤러임을 나타냄
 @RequestMapping("/volunteer/*") // QA 관련 요청을 처리
 @RequiredArgsConstructor // 생성자 자동 생성
 @Slf4j // 로깅 기능 추가
 public class VolunteerController {
-
+    private final PostService postService;
     private final VolunteerService volunteerService;
 
     @GetMapping("volunteer-write")
@@ -50,21 +47,13 @@ public class VolunteerController {
     }
 
     @GetMapping("/volunteer-list")
-    public List<VolunteerDTO> admin(Pagination pagination, Model model) {
-        // volunteerService.getTotal()을 통해 총 데이터 개수를 가져와 pagination에 설정
-        pagination.setTotal(volunteerService.getTotal());
-
-        // pagination 객체의 페이지 계산 진행
+    public String getList(Pagination pagination, Model model) {
+        pagination.setTotal(postService.getTotal("VOLUNTEER"));
         pagination.vtProgress();
+        model.addAttribute("lists", volunteerService.getList(pagination));
 
-        // 설정된 pagination 객체를 로그로 출력하여 확인 (선택 사항)
-        System.out.println("Pagination after vtProgress: " + pagination);
-
-        // volunteer 리스트를 가져와 반환
-        return volunteerService.getList(pagination);
+        return "volunteer/volunteer-list";
     }
-
-
 
 
     @GetMapping("/volunteer-info")
@@ -73,7 +62,7 @@ public class VolunteerController {
         Pagination pagination = new Pagination();
 
         pagination.setOrder(order);
-        pagination.setTotal(volunteerService.getTotal());
+        pagination.setTotal(postService.getTotal("VOLUNTEER"));
         pagination.vtProgress();
         log.info(pagination.toString());
 
@@ -88,8 +77,8 @@ public class VolunteerController {
         return volunteerList;
     }
 
-
-
 }
+
+
 
 
