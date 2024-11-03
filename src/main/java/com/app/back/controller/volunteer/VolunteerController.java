4931e6e4cd3a343b0361c09e7c1d0ad7330bc483
hmk4079 <hmk4079@gmail.com>
2024-11-04 06:49:18 +0900

    init

--- src/main/java/com/app/back/controller/volunteer/VolunteerController.java
+++ src/main/java/com/app/back/controller/volunteer/VolunteerController.java
@@ -49,8 +49,6 @@ public class VolunteerController {
     @GetMapping("/volunteer-list")
     public String getList(Pagination pagination, Model model) {
         pagination.setTotal(postService.getTotal("VOLUNTEER"));
-        pagination.setTotal(30);
-        pagination.setRowCount(2);
         pagination.vtProgress();
         model.addAttribute("lists", volunteerService.getList(pagination));
 
