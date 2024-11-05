15bd197886344cdb76c74a04ed766a5c22b85854
yunjuhyeon <ka06251@naver.com>
2024-11-05 21:38:07 +0900

    clear

--- src/main/java/com/app/back/mapper/post/PostMapper.java
+++ src/main/java/com/app/back/mapper/post/PostMapper.java
@@ -1,9 +1,16 @@
 package com.app.back.mapper.post;
 
+import com.app.back.domain.inquiry.InquiryDTO;
+import com.app.back.domain.post.Pagination;
+import com.app.back.domain.post.PostDTO;
 import com.app.back.domain.post.PostVO;
+import com.app.back.domain.post.Search;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Optional;
+
 @Mapper
 public interface PostMapper {
     //    게시글 작성
@@ -29,4 +36,11 @@ public interface PostMapper {
 
     //    조회수 증가
     public void increaseViewCountById(Long id);
+
+    //  전체조회
+    public List<PostDTO> selectAll(@Param("pagination") Pagination pagination, @Param("search") Search search);
+
+    //    조회
+    public Optional<PostDTO> selectByPostId(Long id);
+
 }
