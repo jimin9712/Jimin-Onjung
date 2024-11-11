389a2b1528b7e4c39a1922550cd4e7f71737bf92
yunjuhyeon <ka06251@naver.com>
2024-11-11 11:01:36 +0900

    clear

--- src/main/resources/static/js/admin/admin_service.js
+++ src/main/resources/static/js/admin/admin_service.js
@@ -197,12 +197,22 @@ fetchRecentNotices();
 //===============================게시글 목록======================================================================
 // 필터링된 게시글 데이터를 가져오는 함수
 const fetchFilteredPosts = async (page = 1, keyword = postKeyword, filterType = postFilterType) => {
+    try {
         const response = await fetch(`/admin/post-list?page=${page}&query=${keyword}&filterType=${filterType}`);
         console.log("서버 응답 상태:", response.ok); // 서버 응답 상태를 확인
+        if (response.ok) {
             const data = await response.json();
+            console.log("서버로부터 받은 데이터:", data); // 받은 데이터 확인
             renderPosts(data.posts); // 필터링된 데이터를 렌더링
             postPagination(data.pagination, keyword, filterType);
+            console.log("필터링 조건:", { page, keyword, filterType });
             resetSelectAllPostsCheckbox(); // 전체 선택 체크박스 해제
+        } else {
+            console.error("서버 응답 실패:", response.status);
+        }
+    } catch (error) {
+        console.error("필터링 오류:", error); // 오류 처리
+    }
 };
 
 
@@ -256,7 +266,7 @@ const fetchPostDetail = async (postId) => {
     }
 };
 
-// =============================== 신고 목록=====================================
+// =========================================================================== 신고 목록=====================================
 // 필터링된 신고 데이터를 가져오는 함수
 const fetchFilteredReports = async (page = 1, keyword = reportKeyword, filterType = reportFilterType) => {
     try {
