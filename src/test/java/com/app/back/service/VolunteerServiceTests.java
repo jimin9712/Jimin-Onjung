54315c03b3b0b7604bf662d0e920661673b54b55
hmk4079 <hmk4079@gmail.com>
2024-11-11 09:17:19 +0900

    init

--- src/test/java/com/app/back/service/VolunteerServiceTests.java
+++ src/test/java/com/app/back/service/VolunteerServiceTests.java
@@ -1,15 +1,11 @@
 package com.app.back.service;
 
-import com.app.back.domain.review.ReviewDTO;
 import com.app.back.domain.volunteer.VolunteerDTO;
-import com.app.back.domain.vt_application.VtApplicationDTO;
-import com.app.back.mapper.volunteer.VolunteerMapper;
 import com.app.back.service.volunteer.VolunteerService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 
